7081e1f679910b4ef423608bd85bcded2f56dbd6
pretzelogic <yingyangyo266@gmail.com>
2022-10-20 03:17:09 -0500

    fix js

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)) ;;enable js2-mode to js files
+(add-to-list 'auto-mode-alist '("\\.js" . js2-mode)) ;;enable js2-mode to js files
 
-(add-to-list 'load-path "~/.emacs.d/packages/")
+(add-to-list 'load-path "~/.emacs.d/packages/");;add packages folder to load path
+
+(setq inhibit-splash-screen t)
 
 (require 'autopair)
 (autopair-global-mode)
